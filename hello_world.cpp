0c12337cc2ddca6181fb243da4b760df6d79322d
Martin Sitta <martin.sitta@alumni.fh-aachen.de>
2023-10-10 20:55:05 +0200

    more experiments with GTK4. I have a good idea on how to make the rest of the UI.

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,19 +1,37 @@
 #include <gtk/gtk.h>
+#include <string>
+GtkWidget* generate_box_with_outline(const char* title)
+{
+  GtkWidget* textbox;
+  GtkWidget* box_outline;
+  box_outline = gtk_frame_new(title);
+  textbox = gtk_box_new(GTK_ORIENTATION_VERTICAL, 8);
+  gtk_frame_set_child(GTK_FRAME(box_outline) ,textbox);
+  return box_outline;
+
+}
 
 static void
 activate (GtkApplication* app,
           gpointer        user_data)
 {
+  GtkWidget* grid_manager;
+  grid_manager = gtk_grid_new();
+  gtk_grid_set_column_homogeneous(GTK_GRID(grid_manager), false);
+  std::string varLen = "a";
+  for(int i = 1; i <= 9; i++)
+  {
+    GtkWidget* box_with_outlines;
+    box_with_outlines = generate_box_with_outline(varLen.c_str());
+    gtk_grid_attach(GTK_GRID(grid_manager), box_with_outlines, i%2, i*4, 1, 2); //number parameters go like this: horizontal offset, vertical offset, colspan, rowspan
+    varLen.append("a");
+  }
   GtkWidget* window;
-  GtkWidget* textbox;
-  GtkWidget* text;
-  text = gtk_label_new("Hello World");
-  textbox = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 8);
-  gtk_box_prepend(GTK_BOX(textbox), text);
   window = gtk_application_window_new (app);
   gtk_window_set_title (GTK_WINDOW (window), "CCG");
   gtk_window_set_default_size (GTK_WINDOW (window), 1920, 1080);
-  gtk_window_set_child(GTK_WINDOW(window), textbox);
+  gtk_window_set_child(GTK_WINDOW(window), grid_manager);
+  gtk_window_maximize(GTK_WINDOW(window));
   gtk_window_present (GTK_WINDOW(window));
 }
 
